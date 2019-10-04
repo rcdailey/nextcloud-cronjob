475568a9d794c973761fdf8478ed2c7edbd22f6c
Phil <philipp.zaiser@gmail.com>
2019-10-04 20:00:43 +0200

    Update find-container.sh
    Changing way how to get ID of running Nextcloud container.

--- scripts/find-container.sh
+++ scripts/find-container.sh
@@ -2,12 +2,10 @@
 set -e
 
 if [[ ! -z "$NEXTCLOUD_PROJECT_NAME" ]]; then
-    containerName="${NEXTCLOUD_PROJECT_NAME}_"
+    containerName="${NEXTCLOUD_PROJECT_NAME}"
 else
     matchEnd=","
 fi
-
-containerName="${containerName}${NEXTCLOUD_CONTAINER_NAME}"
 
 # Get the ID of the container so we can exec something in it later
 docker ps --format '{{.Names}},{{.ID}}' | \
